fc2b25cd8774989f3b9360f5a3e87c1145e45488
armfazh <armfazh@cloudflare.com>
2019-06-12 17:38:12 -0700

    Remove vendor folder

--- vendor/cmac/benches/aes128_cmac.rs
+++
@@ -1,7 +0,0 @@
-#![feature(test)]
-#[macro_use]
-extern crate crypto_mac;
-extern crate cmac;
-extern crate aes;
-
-bench!(cmac::Cmac::<aes::Aes128>);
--- vendor/ctr/benches/aes128.rs
+++
@@ -1,8 +0,0 @@
-#![feature(test)]
-#[macro_use] extern crate stream_cipher;
-extern crate aes;
-extern crate ctr;
-
-type Aes128Ctr = ctr::Ctr128<aes::Aes128>;
-
-bench_sync!(Aes128Ctr);

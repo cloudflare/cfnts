a627e7373fde8c23e99f5661de93f6240eb2a8cd
Suphanat Chunhapanya <haxx.pop@gmail.com>
2019-09-03 16:41:22 +0800

    Refactor log initialization in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+// This file is part of cf-nts.
+// Copyright (c) 2019, Cloudflare. All rights reserved.
+// See LICENSE for licensing information.
+
 extern crate lazy_static;
 extern crate log;
 extern crate prometheus;
 extern crate slog;
+extern crate slog_scope;
+extern crate slog_stdlog;
 extern crate sloggers;
 
 mod cfsock;
@@ -14,7 +20,6 @@ mod nts_ke;
 mod rotation;
 
 use slog::{debug, error};
-use slog_stdlog;
 use sloggers::terminal::{Destination, TerminalLoggerBuilder};
 use sloggers::types::Severity;
 use sloggers::Build;
@@ -26,27 +31,50 @@ use crate::nts_ke::server::start_nts_ke_server;
 
 use std::process;
 
-fn main() {
-    let matches = cmd::create_clap_command().get_matches();
+/// Create a logger to be used throughout cfnts.
+fn create_logger<'a>(matches: &clap::ArgMatches<'a>) -> slog::Logger {
     let mut builder = TerminalLoggerBuilder::new();
+
+    // Default severity level is info.
     builder.level(Severity::Info);
+    // Write all logs to stderr.
     builder.destination(Destination::Stderr);
 
-    if matches.is_present("DEBUG") {
+    // If in debug mode, change severity level to debug.
+    if matches.is_present("debug") {
         builder.level(Severity::Debug);
     }
 
-    let logger = builder.build().unwrap();
-    if let Err(e) = slog_stdlog::init() {
-        error!(logger, "slog_stlog could not be initialized with error: {:?}", e);
-        process::exit(1);
-    }
+    // According to `sloggers-0.3.2` source code, the function doesn't return an error at all.
+    // There should be no problem unwrapping here. It has a return type `Result` because it's a
+    // signature for `sloggers::Build` trait.
+    builder.build().expect("BUG: TerminalLoggerBuilder::build shouldn't return an error.")
+}
+
+/// The entry point of cf-nts.
+fn main() {
+    // According to the documentation of `get_matches`, if the parsing fails, an error will be
+    // displayed to the user and the process will exit with an error code.
+    let matches = cmd::create_clap_command().get_matches();
+
+    let logger = create_logger(&matches);
+
+    // After calling this, slog_stdlog will forward all the `log` crate logging to
+    // `slog_scope::logger()`.
+    //
+    // The returned error type is `SetLoggerError` which, according to the lib doc, will be
+    // returned only when `set_logger` has been called already which should be our bug if it
+    // has already been called.
+    //
+    slog_stdlog::init().expect("BUG: `set_logger` has already been called");
+
+    // _scope_guard can be used to reset the global logger. You can do it by just dropping it.
     let _scope_guard = slog_scope::set_global_logger(logger.clone());
 
-    // TODO: remove this if statement when .subcommand_required_else_help(true) works.
-    if let None = matches.subcommand {
-        error!(logger, "You must specify a subcommand, nts-ke or ntp.");
-        process::exit(127);
+    if matches.subcommand.is_none() {
+        eprintln!("Please specify a valid subcommand. Only client, ke-server, and ntp-server \
+                   are supported.");
+        process::exit(1);
     }
 
     if let Some(nts_ke) = matches.subcommand_matches("nts-ke") {

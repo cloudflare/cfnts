9436bfc71ffc3316df89424274ec0ebbbbdccd96
Suphanat Chunhapanya <haxx.pop@gmail.com>
2019-10-07 11:48:00 +0800

    Have scripts/ use cfnts instead of nts

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -2,11 +2,11 @@
 set  -eu -o pipefail
 export RUST_BACKTRACE=1
 sleep 30
-./target/debug/nts nts-client server -c tests/ca.pem > result.txt
+./target/debug/cfnts client server -c tests/ca.pem > result.txt
 awk '{if (NR == 1 && $2 != 1) exit 1}' result.txt
 for i in {1..1000}
 do
-    ./target/debug/nts nts-client server -c tests/ca.pem &
+    ./target/debug/cfnts client server -c tests/ca.pem &
 done
 wait -n
 curl server:8000/metrics | promtool check metrics
--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 5
 date "+%s"
-RUST_BACKTRACE=1 ./target/debug/nts nts-ke tests/nts-ke-config.yaml &
-RUST_BACKTRACE=1 ./target/debug/nts ntp tests/ntp-config.yaml &
-RUST_BACKTRACE=1 ./target/debug/nts ntp tests/ntp-upstream-config.yaml
+RUST_BACKTRACE=1 ./target/debug/cfnts ke-server tests/nts-ke-config.yaml &
+RUST_BACKTRACE=1 ./target/debug/cfnts ntp-server tests/ntp-config.yaml &
+RUST_BACKTRACE=1 ./target/debug/cfnts ntp-server tests/ntp-upstream-config.yaml

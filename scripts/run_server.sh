66c22027bb4595f4a50dd8f34f3a1d48b402fa95
Luke Valenta <lvalenta@cloudflare.com>
2023-06-01 12:23:16 -0400

    Update development environment scripts and images
    - update base images to bookworm
    - use shared multi-stage build for cfnts client and server
      docker-compose services
    - add clippy service to docker-compose for running linter checks
    - remove 'parallel' from run_server.sh script as it's not installed in
      base image
    - install memcache as python3-memcache to fix failing pip installation

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -2,8 +2,6 @@
 sleep 5
 date "+%s"
 
-parallel -j0 <<EOF
-RUST_BACKTRACE=1 ./target/release/cfnts ke-server -f tests/nts-ke-config.yaml
-RUST_BACKTRACE=1 ./target/release/cfnts ntp-server -f tests/ntp-upstream-config.yaml
+RUST_BACKTRACE=1 ./target/release/cfnts ke-server -f tests/nts-ke-config.yaml &
+RUST_BACKTRACE=1 ./target/release/cfnts ntp-server -f tests/ntp-upstream-config.yaml &
 RUST_BACKTRACE=1 ./target/release/cfnts ntp-server -f tests/ntp-config.yaml
-EOF
